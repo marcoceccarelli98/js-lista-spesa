a13ad34d9d5c9fb7bfbc1a96ca9cd05c88217b45
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-06 20:38:13 +0200

    [UPD] Added script for shopping list visualization

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const shoppingList = ['Pane', 'Latte', 'Gelato', 'Fettine di vitello', 'Vino', 'Biscotti'];
+
+let count = 0;
+
+while (count < shoppingList.length) {
+    console.log(shoppingList[count]);
+    count++;
+}
