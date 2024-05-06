63b76283c91c3ccd81ceda09b2018c124dd1a402
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-06 21:29:42 +0200

    [UPD] Added index list visualization

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 'use strict'
 
-const shoppingList = ['Pane', 'Latte', 'Gelato', 'Fettine di vitello', 'Vino', 'Biscotti'];
+//SHOPPING LIST
+const shoppingList = [
+    'Pane',
+    'Latte',
+    'Gelato',
+    'Fettine di vitello',
+    'Vino',
+    'Biscotti'
+];
+
+//LIST ELEMENT
+const elementList = document.querySelector('.list-group');
+
 
 let count = 0;
 
 while (count < shoppingList.length) {
-    console.log(shoppingList[count]);
+    //Create a li element
+    let li = document.createElement('li');
+    //Insert list content to li element
+    li.append(shoppingList[count]);
+    //Insert li to ul
+    elementList.append(li);
+
     count++;
 }
