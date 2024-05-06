fce48c3f6ef2c27d75bf7dec152c34ccba7a3dfc
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-05-06 21:44:36 +0200

    [UPD] Added some details

--- main.js
+++ main.js
@@ -19,8 +19,26 @@ let count = 0;
 while (count < shoppingList.length) {
     //Create a li element
     let li = document.createElement('li');
-    //Insert list content to li element
-    li.append(shoppingList[count]);
+    //Create a input element
+    let input = document.createElement('input');
+    //Create a label element
+    let label = document.createElement('label');
+
+    //Add input attributes
+    input.classList.add('form-check-input', 'me-3');
+    input.setAttribute('type', 'checkbox');
+
+    //Add label attributes
+    input.classList.add('form-check-label');
+    //Insert label list content
+    label.append(shoppingList[count]);
+
+    // Add li attributes
+    li.classList.add('list-group-item');
+    //Insert input and label to li
+    li.appendChild(input);
+    li.appendChild(label);
+
     //Insert li to ul
     elementList.append(li);
 
